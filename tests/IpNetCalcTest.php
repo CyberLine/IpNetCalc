2971185e3eb01a70cb05bd26db499c80cee759be
Alexander Over <alexander.over@check24.de>
2016-05-24 16:44:47 +0200

    - fixed unit tests

--- tests/IpNetCalcTest.php
+++ tests/IpNetCalcTest.php
@@ -10,17 +10,28 @@ namespace IpNetCalc;
  */
 class IpNetCalcTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var IpNetCalc */
+    protected $class;
+
     /**
      * Dataprovider for Test
      * @return array
      */
-    public function dpCalcNetSum() {
+    public function dpCalcNetSum()
+    {
         return [
             [['192.168.0.1', '192.168.2.40'], '192.168.0.0/22'],
             [['2a00:1450:8004::69', '2001:1af8:1:f006::6'], '2000::/4'],
             [['127.0.0.1', '127.0.0.1'], '127.0.0.1/32'],
-            [['', ''], false],
         ];
+    }
+
+    /**
+     *
+     */
+    public function setup()
+    {
+        $this->class = new IpNetCalc();
     }
 
     /**
@@ -28,8 +39,15 @@ class IpNetCalcTest extends \PHPUnit_Framework_TestCase
      * @param $data
      * @param $expected
      */
-    public function testCalcNetSum($data, $expected) {
-        $class = new IpNetCalc;
-        $this->assertEquals($expected, $class->calcNetSum($data));
+    public function testCalcNetSum($data, $expected)
+    {
+        $this->assertEquals($expected, $this->class->calcNetSum($data));
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testCalcNetSumException() {
+        $this->assertEquals([], $this->class->calcNetSum(['', '']));
     }
 }
